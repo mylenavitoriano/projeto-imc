97ab2f7924414fb1b79c9a05074a1bccae4a7913
Mylena Vitoriano <mylenabarbosa08@gmail.com>
2022-05-13 10:56:24 -0300

    Primeiro commit, iniciando o layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'
+import poweredImage from './assets/powered.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = () => {
+  return(
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150}/>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
